e9609807d72a63c92a96de3ef25a5796d6a18939
Janina Alica Mattes <janina.alica.mattes@gmail.com>
2026-08-01 20:41:38 +1000

    src: Cleanup repositories for PostgreSQL

--- backend/src/infrastructure/database/postgres/repositories/auth.py
+++ backend/src/infrastructure/database/postgres/repositories/auth.py
@@ -1,7 +1,7 @@
 from src.core.logger import get_logger
 from src.domain.auth.repository import AuthTokenRepository
 
-logger = get_logger("api-backend.infra.postgres.auth")
+logger = get_logger("api.infra.postgres.auth")
 
 
 class PostgresAuthRepository(AuthTokenRepository): ...
--- backend/src/infrastructure/database/postgres/repositories/credentials.py
+++ backend/src/infrastructure/database/postgres/repositories/credentials.py
@@ -1,5 +1,6 @@
 from uuid import UUID
 
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.logger import get_logger
@@ -9,7 +10,10 @@ from src.infrastructure.database.postgres.mapper.credentials import CredentialsM
 from src.infrastructure.database.postgres.models.credentials import (
     UserCredentials,
 )
-from src.utils.exceptions import CredentialsNotFoundException, PostgreSQLOperationError
+from src.utils.exceptions import (
+    CredentialsNotFoundException,
+    DatabaseOperationException,
+)
 
 logger = get_logger("api.infra.postgres.credentials")
 
@@ -32,15 +36,17 @@ class PostgresCredentialsRepository(CredentialsRepository):
             self.session.add(db_credentials)
             await self.session.commit()
             await self.session.refresh(db_credentials)
-        except Exception as e:
-            logger.exception(
-                f"Failed to create new entry for user credentials with ID {credentials.credentials_id!r}!",
+        except SQLAlchemyError as exc:
+            logger.warning(
+                "Raised database related error for %s for user %s. This could be due to an invalid or conflicting function argument.",
+                credentials.credentials_id,
+                credentials.user_id,
             )
             await self.session.rollback()
-            raise PostgreSQLOperationError(
-                "PostgreSQL DB Error",
-                f"Failed to create new entry for user credentials with ID {credentials.credentials_id!r}!",
-            ) from e
+
+            raise DatabaseOperationException(
+                f"Failed to store new credentials for '{credentials.credentials_id}' in database."
+            ) from exc
 
         return CredentialsMapper.to_entity(db_credentials)  # after refresh
 
@@ -48,20 +54,19 @@ class PostgresCredentialsRepository(CredentialsRepository):
         """Retrieve a record by its primary key."""
         try:
             db_credentials = await self.session.get(UserCredentials, credentials_id)
-            if db_credentials is None:
-                raise CredentialsNotFoundException(
-                    name="Credentials Repository Error",
-                    message=f"Requested document with ID {credentials_id} not found!",
-                )
-        except Exception as e:
-            logger.exception(
-                f"Failed to get user credentials with ID {credentials_id!r}!",
+        except SQLAlchemyError as exc:
+            logger.warning(
+                "Raised database related error for %s. This could be due to an invalid or conflicting function argument.",
+                credentials_id,
             )
             await self.session.rollback()
-            raise PostgreSQLOperationError(
-                "PostgreSQL DB Error",
-                f"Failed to get user credentials with ID {credentials_id!r}!",
-            ) from e
+
+            raise DatabaseOperationException(
+                f"Failed to read credentials for '{credentials_id}' in database."
+            ) from exc
+
+        if db_credentials is None:
+            raise CredentialsNotFoundException(credentials_id=credentials_id)
 
         return CredentialsMapper.to_entity(db_credentials)
 
@@ -76,15 +81,17 @@ class PostgresCredentialsRepository(CredentialsRepository):
             merged_credentials = await self.session.merge(db_credentials)
             await self.session.commit()
             await self.session.refresh(merged_credentials)
-        except Exception as e:
-            logger.exception(
-                f"Failed to update user credentials with ID {credentials.credentials_id!r}!",
+        except SQLAlchemyError as exc:
+            logger.warning(
+                "Raised database related error for %s for user %s. This could be due to an invalid or conflicting function argument.",
+                credentials.credentials_id,
+                credentials.user_id,
             )
             await self.session.rollback()
-            raise PostgreSQLOperationError(
-                "PostgreSQL DB Error",
-                f"Failed to update user credentials with ID {credentials.credentials_id!r}!",
-            ) from e
+
+            raise DatabaseOperationException(
+                f"Failed to update credentials for '{credentials.credentials_id}' in database."
+            ) from exc
 
         return CredentialsMapper.to_entity(merged_credentials)  # after refresh
 
@@ -99,14 +106,16 @@ class PostgresCredentialsRepository(CredentialsRepository):
             merged_credentials = await self.session.merge(db_credentials)
             await self.session.delete(merged_credentials)
             await self.session.commit()
-        except Exception as e:
-            logger.exception(
-                f"Failed to update user credentials with ID {credentials.credentials_id!r}!",
+        except SQLAlchemyError as exc:
+            logger.warning(
+                "Raised database related error for %s for user %s. This could be due to an invalid or conflicting function argument.",
+                credentials.credentials_id,
+                credentials.user_id,
             )
             await self.session.rollback()
-            raise PostgreSQLOperationError(
-                "PostgreSQL DB Error",
-                f"Failed to update user credentials with ID {credentials.credentials_id!r}!",
-            ) from e
+
+            raise DatabaseOperationException(
+                f"Failed to remove credentials for '{credentials.credentials_id}' in database."
+            ) from exc
 
         return CredentialsMapper.to_entity(merged_credentials)  # after refresh
