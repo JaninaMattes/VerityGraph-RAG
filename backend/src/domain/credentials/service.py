e2ac0a8096ff011853a87479e35b61ef9f5930b8
Janina Alica Mattes <janina.alica.mattes@gmail.com>
2026-08-01 20:36:23 +1000

    src: Update logging and exception handling in business logic credentials

--- backend/src/domain/credentials/service.py
+++ backend/src/domain/credentials/service.py
@@ -1,4 +1,17 @@
-class CredentialService:
-    """The service layer defines the authentication business logic
-    that ineracts with the repository."""
+from src.domain.credentials.repository import CredentialsRepository
+from src.shared.enums import StorageProvider
 
+
+class CredentialService:
+    """
+    The service layer defines the authentication business logic
+    that ineracts with the repository.
+    """
+
+    def __init__(
+        self,
+        repository: CredentialsRepository,
+        storage: StorageProvider,
+    ) -> None:
+        self.repository = repository
+        self.storage = storage
