c9730595a3cac1df8145faac916e02ed0a4766c3
Janina Alica Mattes <janina.alica.mattes@gmail.com>
2026-08-03 16:15:01 +1000

    src: Update alembic

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,14 +1,13 @@
 import asyncio
 from logging.config import fileConfig
 
+import src.infrastructure.database.postgres.models  # noqa: F401
+from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
-
-import src.infrastructure.database.postgres.models  # noqa: F401
-from alembic import context
-from src.core.config import get_settings
 from src.infrastructure.database.postgres.base import Base
+from src.shared.core.config import get_settings
 
 settings = get_settings()
 # this is the Alembic Config object, which provides
