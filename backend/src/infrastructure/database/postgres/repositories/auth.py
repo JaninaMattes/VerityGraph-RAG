575f1a60ebdf56eb9ad6fb86b5c98a3b2a51ff2c
Janina Alica Mattes <janina.alica.mattes@gmail.com>
2026-09-10 18:42:48 +1000

    src: Remove infrastructure for authentification in DB

--- backend/src/infrastructure/database/postgres/repositories/auth.py
+++
@@ -1,7 +0,0 @@
-from src.shared.core.logger import get_logger
-from src.domain.auth.repository import AuthTokenRepository
-
-logger = get_logger("api.infra.postgres.auth")
-
-
-class PostgresAuthRepository(AuthTokenRepository): ...
