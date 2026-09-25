35da27a24af05b85d4e045978f3ab91347570f4a
Janina Alica Mattes <janina.alica.mattes@gmail.com>
2026-09-25 22:20:10 +1000

    src: Update app settings in config

--- backend/src/shared/core/config.py
+++ backend/src/shared/core/config.py
@@ -61,7 +61,8 @@ class Settings(BaseSettings):
     # ---------------------------------------------------------
 
     kafka_url: str
-    kafka_topics: str = "minio-events"
+    kafka_topics: list[str] = ["minio-events"]
+    kafka_bootstrap_servers: str = "localhost"
     kafka_group_id: str = "minio-ingestion-group"
     kafka_auto_offset_reset: str = "earliest"
     kafka_enable_auto_commit: bool = False  # manually commit
